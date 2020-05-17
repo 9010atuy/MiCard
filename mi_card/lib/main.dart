4f86bddc5b87b09b262f21ae923df08d825d85a7
yuta <9010atuy@gmail.copm>
2020-05-18 00:23:44 +0900

    layout challenge

--- mi_card/lib/main.dart
+++ mi_card/lib/main.dart
@@ -11,15 +11,35 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.tealAccent,
+        backgroundColor: Colors.teal,
         body: SafeArea(
-          child: Container(
-            child: Text('The Viking Of Kings'),
-            color: Colors.white,
-            height: 100.0,
-            width: 200.0,
-            padding: EdgeInsets.all(10.0),
-            margin: EdgeInsets.fromLTRB(20.0, 30.0, 40.0, 20.0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: <Widget>[
+              Container(
+                color: Colors.red,
+                width: 100.0,
+              ),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Container(
+                    width: 100.0,
+                    height: 100.0,
+                    color: Colors.yellow,
+                  ),
+                  Container(
+                    width: 100.0,
+                    height: 100.0,
+                    color: Colors.lightGreen,
+                  )
+                ],
+              ),
+              Container(
+                color: Colors.blue,
+                width: 100.0,
+              ),
+            ],
           ),
         ),
       ),
