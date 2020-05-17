e1e23bcc2831658c0c5cde79b409b2088614fc25
yuta <9010atuy@gmail.copm>
2020-05-17 22:58:01 +0900

    How to Container

--- mi_card/lib/main.dart
+++ mi_card/lib/main.dart
@@ -11,8 +11,17 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        backgroundColor: Colors.white,
-        body: Container(),
+        backgroundColor: Colors.tealAccent,
+        body: SafeArea(
+          child: Container(
+            child: Text('The Viking Of Kings'),
+            color: Colors.white,
+            height: 100.0,
+            width: 200.0,
+            padding: EdgeInsets.all(10.0),
+            margin: EdgeInsets.fromLTRB(20.0, 30.0, 40.0, 20.0),
+          ),
+        ),
       ),
     );
   }
